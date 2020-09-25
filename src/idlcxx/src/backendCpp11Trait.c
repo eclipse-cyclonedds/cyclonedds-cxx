46e8123d859136d3d6462816cded3b34b9d0c4f1
Erik Hendriks <erik.hendriks@adlinktech.com>
2020-09-25 09:15:59 +0200

    Avoid generation of duplicate invocations of the REGISTER_TOPIC_TYPES macro.
    The walk_tree function has been modified to only dive into a subtree when
    the targeted node matches the specified mask. That means that callers may
    need to extend their masks to include the types of all parent nodes leading
    up to the targeted nodes.
    
    Signed-off-by: Erik Hendriks <erik.hendriks@adlinktech.com>

--- src/idlcxx/src/backendCpp11Trait.c
+++ src/idlcxx/src/backendCpp11Trait.c
@@ -87,14 +87,32 @@ generate_traits(idl_backend_ctx ctx, const idl_node_t *node)
 static idl_retcode_t
 generate_macro_call(idl_backend_ctx ctx, const idl_node_t *node)
 {
-  assert(node->mask & IDL_STRUCT);
+  if (node->mask & IDL_STRUCT)
+  {
+    char *struct_name = get_cpp11_fully_scoped_name(node);
+    idl_file_out_printf(ctx, "REGISTER_TOPIC_TYPE(%s)\n", struct_name);
+    free(struct_name);
+  }
 
-  char *struct_name = get_cpp11_fully_scoped_name(node);
+  return IDL_RETCODE_OK;
+}
 
-  idl_file_out_printf(ctx, "REGISTER_TOPIC_TYPE(%s)\n", struct_name);
+static idl_retcode_t
+find_topic_types(idl_backend_ctx ctx, const idl_node_t *node)
+{
+  idl_walkAction *action = (idl_walkAction*) idl_get_custom_context(ctx);
 
-  free(struct_name);
-
+  switch (node->mask & (IDL_MODULE | IDL_STRUCT))
+  {
+  case IDL_MODULE:
+    idl_walk_node_list(ctx, ((const idl_module_t *) node)->definitions, find_topic_types, IDL_MODULE | IDL_STRUCT);
+    break;
+  case IDL_STRUCT:
+    action(ctx, node);
+    break;
+  default:
+    break;
+  }
   return IDL_RETCODE_OK;
 }
 
@@ -108,9 +126,13 @@ idl_backendGenerateTrait(idl_backend_ctx ctx, const idl_tree_t *parse_tree)
   idl_file_out_printf(ctx, "#include \"org/eclipse/cyclonedds/topic/DataRepresentation.hpp\"\n");
   idl_file_out_printf(ctx, "#include \"org/eclipse/cyclonedds/topic/datatopic.hpp\"\n\n");
   idl_file_out_printf(ctx, "namespace org { namespace eclipse { namespace cyclonedds { namespace topic {\n");
-  result = idl_walk_tree(ctx, parse_tree->root, generate_traits, IDL_STRUCT);
+  idl_set_custom_context(ctx, (void *) generate_traits);
+  result = idl_walk_node_list(ctx, parse_tree->root, find_topic_types, IDL_STRUCT | IDL_MODULE);
+  idl_reset_custom_context(ctx);
+  idl_set_custom_context(ctx, (void *) generate_macro_call);
   idl_file_out_printf(ctx, "}}}}\n\n");
-  result = idl_walk_tree(ctx, parse_tree->root, generate_macro_call, IDL_STRUCT);
+  result = idl_walk_node_list(ctx, parse_tree->root, find_topic_types, IDL_MODULE | IDL_STRUCT);
+  idl_reset_custom_context(ctx);
   idl_file_out_printf(ctx, "\n");
 
   return result;
