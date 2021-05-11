1cd7dfea35a5021e5c2e7138fa72430464d9ea01
Jeroen Koekkoek <jeroen@koekkoek.nl>
2021-05-11 13:33:36 +0200

    Fix warnings regarding casts in copy constructors
    Signed-off-by: Jeroen Koekkoek <jeroen@koekkoek.nl>

--- src/ddscxx/include/dds/sub/cond/detail/TReadConditionImpl.hpp
+++ src/ddscxx/include/dds/sub/cond/detail/TReadConditionImpl.hpp
@@ -92,7 +92,8 @@ template <typename DELEGATE>
 TCondition<DELEGATE>&
 TCondition<DELEGATE>::operator=(const dds::sub::cond::TReadCondition<org::eclipse::cyclonedds::sub::cond::ReadConditionDelegate>& rhs)
 {
-    if (this != static_cast<TCondition*>(&rhs)) {
+    const TCondition<DELEGATE> &t = rhs;
+    if (this != &t) {
         if (rhs.is_nil()) {
             /* We got a null object and are not really able to do a typecheck here. */
             /* So, just set a null object. */
