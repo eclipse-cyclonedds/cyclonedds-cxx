0799214d227475f332ec33d2871c6b44ec08897a
Sumanth Nirmal <sumanth.724@gmail.com>
2021-07-07 16:17:15 -0700

    Rework the shared memory tests
      - Include additional types for tests
      - Add tests for is_loan_available
      - Add tests for loan_sample, return_sample APIs
    
    Signed-off-by: Sumanth Nirmal <sumanth.724@gmail.com>

--- src/ddscxx/tests/SharedMemory.cpp
+++ src/ddscxx/tests/SharedMemory.cpp
@@ -16,12 +16,23 @@
 #include "iceoryx_posh/testing/roudi_gtest.hpp"
 #include "HelloWorldData.hpp"
 #include "Space.hpp"
+#include "Bounded.hpp"
+
+#define EXPECT_THROW_EXCEPTION(statement, error_msg) \
+  ASSERT_THROW(statement, dds::core::Exception); \
+  try { \
+    statement; \
+  } catch (const dds::core::Exception & err) { \
+    EXPECT_NE(std::string(err.what()).find(error_msg), std::string::npos); \
+  }
 
 namespace
 {
 template<class T>
 void make_sample_(T & sample, const int32_t cnt)
 {
+  (void) sample;
+  (void) cnt;
   throw std::runtime_error("make_sample_ called on unsupported type");
 }
 
@@ -40,47 +51,14 @@ void make_sample_(HelloWorldData::Msg & sample, const int32_t cnt)
   sample.message(std::to_string(cnt));
 }
 
-template<class T>
-std::vector<T> write_loaned_data_(dds::pub::DataWriter<T> & writer, const int32_t cnt)
-{
-  std::vector<T> samples;
-  for (int32_t i = 0; i < cnt; i++) {
-    // loan memory from the middleware
-    auto & loaned_sample = writer.delegate()->loan_sample();
-    // make sample
-    make_sample_(loaned_sample, i);
-    // store sample for comparison later
-    samples.push_back(loaned_sample);
-    // write sample (which will also release the loan to the middleware)
-    writer.write(loaned_sample);
-  }
-  return samples;
-}
-
 template<>
-std::vector<HelloWorldData::Msg> write_loaned_data_(
-  dds::pub::DataWriter<HelloWorldData::Msg> & writer, const int32_t cnt)
+void make_sample_(Bounded::Msg & sample, const int32_t cnt)
 {
-  // loaning on a non-fixed type throws an error
-  try {
-    auto & loaned_sample = writer.delegate()->loan_sample();
-    (void)loaned_sample;
-    EXPECT_FALSE(true);
-  } catch (const std::exception & e) {
-    EXPECT_STREQ(e.what(), "sample loan failed");
-  }
-
-  std::vector<HelloWorldData::Msg> samples;
-  for (int32_t i = 0; i < cnt; i++) {
-    HelloWorldData::Msg sample;
-    // make sample
-    make_sample_(sample, i);
-    // store sample for comparison later
-    samples.push_back(sample);
-    // write sample (which will also release the loan to the middleware)
-    writer.write(sample);
-  }
-  return samples;
+  sample.bounded_string(std::to_string(cnt));
+  sample.boolean_sequence().reserve(255);
+  std::fill(sample.boolean_sequence().begin(), sample.boolean_sequence().begin() + 255, true);
+  sample.bounded_sequence().reserve(255);
+  std::fill(sample.bounded_sequence().begin(), sample.bounded_sequence().begin() + 255, cnt);
 }
 }
 /**
@@ -187,24 +165,34 @@ public:
     EXPECT_NO_THROW(waitset.wait(dds::core::Duration(10, 0)));
   }
 
-  std::vector<T> WriteData(int32_t instances_cnt)
+  std::vector<T> WriteData(const int32_t instances_cnt)
   {
     std::vector<T> samples;
-    for (int32_t i = 0; i < instances_cnt; i++) {
-      // make sample
-      T sample;
-      make_sample_(sample, i);
-      // write sample
-      this->writer.write(sample);
-      // store sample
-      samples.push_back(sample);
+    // if writer supports loaning
+    if (writer.delegate()->is_loan_supported()) {
+      for (int32_t i = 0; i < instances_cnt; i++) {
+        // loan memory from the middleware
+        auto & loaned_sample = writer.delegate()->loan_sample();
+        // make sample
+        make_sample_(loaned_sample, i);
+        // store sample for comparison later
+        samples.push_back(loaned_sample);
+        // write sample (which will also release the loan to the middleware)
+        writer.write(loaned_sample);
+      }
+      return samples;
+    } else {
+      for (int32_t i = 0; i < instances_cnt; i++) {
+        // make sample
+        T sample;
+        make_sample_(sample, i);
+        // write sample
+        this->writer.write(sample);
+        // store sample
+        samples.push_back(sample);
+      }
+      return samples;
     }
-    return samples;
-  }
-
-  std::vector<T> WriteLoanedData(int32_t instances_cnt)
-  {
-    return write_loaned_data_<T>(this->writer, instances_cnt);
   }
 
   void
@@ -230,11 +218,10 @@ public:
     }
   }
 
-  void run_test(
+  void run_communication_test(
     const dds::sub::qos::DataReaderQos & r_qos,
     const dds::pub::qos::DataWriterQos & w_qos,
-    const int32_t num_samples,
-    const bool use_loaning = false)
+    const int32_t num_samples)
   {
     dds::sub::LoanedSamples<T> samples;
     std::vector<T> test_samples;
@@ -242,16 +229,20 @@ public:
     /* setup communication */
     this->SetupCommunication(r_qos, w_qos);
     /* write data. */
-    if (use_loaning) {
-      test_samples = this->WriteLoanedData(num_samples);
-    } else {
-      test_samples = this->WriteData(num_samples);
-    }
+    test_samples = this->WriteData(num_samples);
     /* wait for data */
     this->WaitForData();
     /* Check result by taking. */
     samples = this->reader.take();
     this->CheckData(samples, test_samples);
+  }
+
+  void run_loan_support_api_test(const bool valid_r_shm_qos, const bool valid_w_shm_qos)
+  {
+    EXPECT_EQ(this->reader.delegate()->is_loan_supported(),
+      (org::eclipse::cyclonedds::topic::TopicTraits<T>::isSelfContained() && valid_r_shm_qos));
+    EXPECT_EQ(this->writer.delegate()->is_loan_supported(),
+      (org::eclipse::cyclonedds::topic::TopicTraits<T>::isSelfContained() && valid_w_shm_qos));
   }
 
   void TearDown()
@@ -269,81 +260,110 @@ public:
  * Tests
  */
 
-using TestTypes = testing::Types<Space::Type1, HelloWorldData::Msg>;
+using TestTypes = testing::Types<Space::Type1, HelloWorldData::Msg, Bounded::Msg>;
 TYPED_TEST_CASE(SharedMemoryTest, TestTypes);
 
 TYPED_TEST(SharedMemoryTest, writer_reader_valid_shm_qos)
 {
+  // valid SHM QoS
   dds::sub::qos::DataReaderQos r_qos;
   r_qos << dds::core::policy::Reliability::BestEffort();
   r_qos << dds::core::policy::Durability::Volatile();
   r_qos << dds::core::policy::History::KeepLast(10U);
+  constexpr bool valid_r_shm_qos = true;
 
+  // valid SHM QoS
   dds::pub::qos::DataWriterQos w_qos;
   w_qos << dds::core::policy::Reliability::Reliable();
   w_qos << dds::core::policy::Durability::Volatile();
   w_qos << dds::core::policy::History::KeepLast(10U);
+  constexpr bool valid_w_shm_qos = true;
 
-  this->run_test(r_qos, w_qos, 10);
+  // tests
+  this->run_communication_test(r_qos, w_qos, 10);
+  this->run_loan_support_api_test(valid_r_shm_qos, valid_w_shm_qos);
 }
 
 TYPED_TEST(SharedMemoryTest, writer_reader_default_qos)
 {
+  // default Qos (is valid SHM qos)
   dds::sub::qos::DataReaderQos r_qos{};
   dds::pub::qos::DataWriterQos w_qos{};
+  constexpr bool valid_shm_qos = true;
 
-  this->run_test(r_qos, w_qos, 1);
+  // test communication
+  this->run_communication_test(r_qos, w_qos, 1);
+  this->run_loan_support_api_test(valid_shm_qos, valid_shm_qos);
 }
 
 TYPED_TEST(SharedMemoryTest, writer_valid_shm_qos)
 {
+  // valid reader shm qos
   dds::sub::qos::DataReaderQos r_qos;
-  r_qos << dds::core::policy::Reliability::BestEffort();
+  r_qos << dds::core::policy::Reliability::Reliable();
+  // to not enable SHM for the reader, we should use transient local, but volatile writer and
+  // transient local reader is not a valid QoS
   r_qos << dds::core::policy::Durability::Volatile();
   r_qos << dds::core::policy::History::KeepLast(10U);
+  constexpr bool valid_r_shm_qos = true;
 
+  // valid writer shm qos
   dds::pub::qos::DataWriterQos w_qos;
   w_qos << dds::core::policy::Reliability::Reliable();
   w_qos << dds::core::policy::Durability::Volatile();
   w_qos << dds::core::policy::History::KeepLast(10U);
+  constexpr bool valid_w_shm_qos = true;
 
-  this->run_test(r_qos, w_qos, 10);
+  // tests
+  this->run_communication_test(r_qos, w_qos, 10);
+  this->run_loan_support_api_test(valid_r_shm_qos, valid_w_shm_qos);
 }
 
 TYPED_TEST(SharedMemoryTest, reader_valid_shm_qos)
 {
+  // valid reader shm qos
   dds::sub::qos::DataReaderQos r_qos;
-  r_qos << dds::core::policy::Reliability::BestEffort();
+  r_qos << dds::core::policy::Reliability::Reliable();
   r_qos << dds::core::policy::Durability::Volatile();
   r_qos << dds::core::policy::History::KeepLast(10U);
+  constexpr bool valid_r_shm_qos = true;
 
+  // invalid writer shm qos
   dds::pub::qos::DataWriterQos w_qos;
   w_qos << dds::core::policy::Reliability::Reliable();
   w_qos << dds::core::policy::Durability::TransientLocal();
   w_qos << dds::core::policy::History::KeepLast(10U);
+  constexpr bool valid_w_shm_qos = false;
 
-  this->run_test(r_qos, w_qos, 10);
+  // tests
+  this->run_communication_test(r_qos, w_qos, 10);
+  this->run_loan_support_api_test(valid_r_shm_qos, valid_w_shm_qos);
 }
 
 TYPED_TEST(SharedMemoryTest, invalid_shm_qos)
 {
+  // invalid reader shm QoS
   dds::sub::qos::DataReaderQos r_qos;
-  r_qos << dds::core::policy::Reliability::BestEffort();
+  r_qos << dds::core::policy::Reliability::Reliable();
   r_qos << dds::core::policy::Durability::TransientLocal();
   r_qos << dds::core::policy::History::KeepLast(10U);
+  constexpr bool valid_r_shm_qos = false;
 
+  // invalid writer SHM QoS
   dds::pub::qos::DataWriterQos w_qos;
   w_qos << dds::core::policy::Reliability::Reliable();
   w_qos << dds::core::policy::Durability::TransientLocal();
   w_qos << dds::core::policy::History::KeepLast(10U);
+  constexpr bool valid_w_shm_qos = false;
 
-  this->run_test(r_qos, w_qos, 10);
+  this->run_communication_test(r_qos, w_qos, 10);
+  this->run_loan_support_api_test(valid_r_shm_qos, valid_w_shm_qos);
 }
 
 TYPED_TEST(SharedMemoryTest, loan_sample)
 {
   dds::sub::qos::DataReaderQos r_qos;
-  r_qos << dds::core::policy::Reliability::BestEffort();
+  r_qos << dds::core::policy::Reliability::Reliable();
   r_qos << dds::core::policy::Durability::Volatile();
   r_qos << dds::core::policy::History::KeepLast(10U);
 
@@ -352,5 +372,27 @@ TYPED_TEST(SharedMemoryTest, loan_sample)
   w_qos << dds::core::policy::Durability::Volatile();
   w_qos << dds::core::policy::History::KeepLast(10U);
 
-  this->run_test(r_qos, w_qos, 10, true);
+  this->SetupCommunication(r_qos, w_qos);
+  using DDSType = typename TestFixture::TopicType;
+  // request loan
+  if (org::eclipse::cyclonedds::topic::TopicTraits<DDSType>::isSelfContained()) {
+    try {
+      auto & loaned_sample = this->writer.delegate()->loan_sample();
+
+      // return the loan back without publishing
+      this->writer.delegate()->return_loan(loaned_sample);
+    } catch (...) {
+      // should never execute this
+      EXPECT_TRUE(false);
+    }
+  } else {  // when loaning is not supported
+    EXPECT_THROW_EXCEPTION(this->writer.delegate()->loan_sample(),
+      "Error Unsupported - sample loan failed.");
+
+    // illegal loan
+    DDSType sample;
+    EXPECT_THROW_EXCEPTION(
+      this->writer.delegate()->return_loan(sample),
+      "Error Unsupported - return of sample loan failed.");
+  }
 }
