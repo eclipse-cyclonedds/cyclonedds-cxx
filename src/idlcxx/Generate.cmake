e1305339cbcdefc640a29f22006375be989ce097
Matija Tudan <tudan.matija@gmail.com>
2021-12-14 14:37:28 +0100

    idlcxx: use idlc_generate_generic() function from CycloneDDS package
    This commit allows to use generic function from cyclonedds repo
    instead of implementing almost the same thing in this repo again.
    
    Signed-off-by: Matija Tudan <tudan.matija@gmail.com>

--- src/idlcxx/Generate.cmake
+++ src/idlcxx/Generate.cmake
@@ -9,60 +9,35 @@
 #
 # SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
 #
+
 function(IDLCXX_GENERATE)
   set(one_value_keywords TARGET)
-  set(multi_value_keywords FILES FEATURES)
+  set(multi_value_keywords FILES FEATURES INCLUDES)
   cmake_parse_arguments(
     IDLCXX "" "${one_value_keywords}" "${multi_value_keywords}" "" ${ARGN})
 
-  if(NOT IDLCXX_TARGET AND NOT IDLCXX_FILES)
-    # assume deprecated invocation: TARGET FILE [FILE..]
-    list(GET IDLCXX_UNPARSED_ARGUMENTS 0 IDLCXX_TARGET)
-    list(REMOVE_AT IDLCXX_UNPARSED_ARGUMENTS 0)
-    set(IDLCXX_FILES ${IDLCXX_UNPARSED_ARGUMENTS})
-    if (IDLCXX_TARGET AND IDLCXX_FILES)
-      message(WARNING " Deprecated use of idlcxx_generate. \n"
-                      " Consider switching to keyword based invocation.")
+  find_package(CycloneDDS REQUIRED)
+
+  # find idlcxx shared library
+  if(CMAKE_CROSSCOMPILING)
+    find_library(_idlcxx_shared_lib cycloneddsidlcxx NO_CMAKE_FIND_ROOT_PATH REQUIRED)
+
+    if(_idlcxx_shared_lib)
+      set(_idlcxx_depends "")
+    else()
+      message(FATAL_ERROR "Cannot find idlcxx shared library")
     endif()
-    # Java based compiler used to be case sensitive
-    list(APPEND IDLCXX_FEATURES "case-sensitive")
+  else()
+    set(_idlcxx_shared_lib "$<TARGET_FILE:CycloneDDS-CXX::idlcxx>")
+    set(_idlcxx_depends CycloneDDS-CXX::idlcxx)
   endif()
 
-  if(NOT IDLCXX_TARGET)
-    message(FATAL_ERROR "idlcxx_generate called without TARGET")
-  elseif(NOT IDLCXX_FILES)
-    message(FATAL_ERROR "idlcxx_generate called without FILES")
-  endif()
-
-  # remove duplicate features
-  if(IDLCXX_FEATURES)
-    list(REMOVE_DUPLICATES IDLCXX_FEATURES)
-  endif()
-  foreach(_feature ${IDLCXX_FEATURES})
-    list(APPEND IDLCXX_ARGS "-f" ${_feature})
-  endforeach()
-
-  set(_dir ${CMAKE_CURRENT_BINARY_DIR})
-  set(_target ${IDLCXX_TARGET})
-  foreach(_file ${IDLCXX_FILES})
-    get_filename_component(_path ${_file} ABSOLUTE)
-    list(APPEND _files "${_path}")
-  endforeach()
-
-  foreach(_file ${_files})
-    get_filename_component(_name ${_file} NAME_WE)
-    set(_header "${_dir}/${_name}.hpp")
-    list(APPEND _headers "${_header}")
-    add_custom_command(
-      OUTPUT   "${_header}"
-      COMMAND  CycloneDDS::idlc
-      ARGS     -l $<TARGET_FILE:CycloneDDS-CXX::idlcxx> ${IDLCXX_ARGS} ${_file}
-      DEPENDS  ${_files} CycloneDDS::idlc CycloneDDS-CXX::idlcxx)
-  endforeach()
-
-  add_custom_target("${_target}_generate" DEPENDS ${_headers})
-  add_library(${_target} INTERFACE)
-  target_sources(${_target} INTERFACE ${_headers})
-  target_include_directories(${_target} INTERFACE "${_dir}")
-  add_dependencies(${_target} "${_target}_generate")
+  idlc_generate_generic(TARGET ${IDLCXX_TARGET}
+    BACKEND ${_idlcxx_shared_lib}
+    FILES ${IDLCXX_FILES}
+    FEATURES ${IDLCXX_FEATURES}
+    INCLUDES ${IDLCXX_INCLUDES}
+    SUFFIXES .hpp
+    DEPENDS ${_idlcxx_depends}
+  )
 endfunction()
