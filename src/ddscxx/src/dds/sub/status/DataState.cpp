52542581421822e9f5341b2017f03250e19134f3
Troy Karan Harrison <troy.harrison@zettascale.tech>
2023-04-28 15:39:11 +0200

    [CP-277] Fix MISRA 3.1 violations in copyright blurbs

--- src/ddscxx/src/dds/sub/status/DataState.cpp
+++ src/ddscxx/src/dds/sub/status/DataState.cpp
@@ -1,14 +1,12 @@
-/*
- * Copyright(c) 2006 to 2020 ZettaScale Technology and others
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
- * v. 1.0 which is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
- */
+// Copyright(c) 2006 to 2020 ZettaScale Technology and others
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+// v. 1.0 which is available at
+// http://www.eclipse.org/org/documents/edl-v10.php.
+//
+// SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
 
 
 /**
@@ -28,22 +26,22 @@ namespace status
 
 SampleState::SampleState() : MaskType() { }
 SampleState::SampleState(uint32_t i) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-     * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
         (i)) { }
 SampleState::SampleState(const SampleState& src) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-     * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
         (src.to_ulong())) { }
 SampleState::SampleState(const MaskType& src) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-    * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
@@ -51,45 +49,45 @@ SampleState::SampleState(const MaskType& src) : MaskType(
 
 ViewState::ViewState() : MaskType() { }
 ViewState::ViewState(uint32_t m) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-    * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
         (m)) { }
 ViewState::ViewState(const ViewState& src) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-    * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
         (src.to_ulong())) { }
 ViewState::ViewState(const MaskType& src) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-    * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
         (src.to_ulong())) { }
 
 InstanceState::InstanceState(uint32_t m) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-    * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
         (m)) { }
 InstanceState::InstanceState() : MaskType() { }
 InstanceState::InstanceState(const InstanceState& src) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-    * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
         (src.to_ulong())) { }
 InstanceState::InstanceState(const MaskType& src) : MaskType(
-    /** @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
-    * @see http://connect.microsoft.com/VisualStudio/feedback/details/532897 */
+    /// @internal @note MSVC bug: Problems constructing a bitset from an unsigned long in the VC RC
+    /// @see http://connect.microsoft.com/VisualStudio/feedback/details/532897
 #if _MSC_VER == 1600
         static_cast<int>
 #endif
